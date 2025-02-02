bb7c22ed10fba820fa4fb74fbedd81c97e3cc8e0
Barry-owino <barryowino5@gmail.com>
2025-02-02 21:23:26 +0300

    updated the alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,17 +1,17 @@
--- Create the database
-CREATE DATABASE alx_book_store;
+--Create Database if it does not exist
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
--- Use the created database
+-- Use the database
 USE alx_book_store;
 
--- Create Authors table
-CREATE TABLE Authors (
+-- Create Authors Table
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
--- Create Books table
-CREATE TABLE Books (
+-- Create Books Table
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
@@ -20,24 +20,24 @@ CREATE TABLE Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- Create Customers table
-CREATE TABLE Customers (
+-- Create Customers Table
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) UNIQUE NOT NULL,
+    email VARCHAR(215) NOT NULL,
     address TEXT
 );
 
--- Create Orders table
-CREATE TABLE Orders (
+-- Create Orders Table
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
-    order_date DATE NOT NULL,
+    order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- Create Order_Details table
-CREATE TABLE Order_Details (
+-- Create Order_Details Table
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
@@ -45,4 +45,3 @@ CREATE TABLE Order_Details (
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
-
