76db72ebc8a2ac065b7091d620f45a65ac37ea2d
Barry-owino <barryowino5@gmail.com>
2025-02-02 22:18:23 +0300

    script that prints the full description of the Books

---
+++ task_4.sql
@@ -0,0 +1,8 @@
+-- Use the alx_book_store database
+USE alx_book_store;
+
+-- Query to retrieve the table structure from information_schema
+SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'Books' AND TABLE_SCHEMA = DATABASE();
+
